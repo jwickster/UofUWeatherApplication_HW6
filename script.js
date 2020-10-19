ae37924cf0c8855a33c98bdee74cac5114099504
Jwickster <joshwickster@gmail.com>
2020-10-19 10:17:32 -0600

    html edits

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Linking variables to jQuery
+//capturing Name, temp, humidity, wind speed, UV index, long/latitude
+var cityName = $(".");
+var cityTemperature = $(".");
+var cityHumidity = $(".");
+var cityWindSpeed = $(".");
+var cityUVIndex = $(".");
+var cityLongitude = $(".");
+var cityLatitude = $(".");
+
+//JSON parse
+var localStorageCity = JSON.parse(localStorage.getItem(cityName));
+
+//function call
+
