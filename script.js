c374b9653a043e4114c18dc6c55abb91320f052a
Josh Wickster <joshwickster@gmail.com>
2020-10-24 16:45:24 -0600

    Added Kelvin to Farenheit conversion

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ weatherApp(cityFromLocalStorage);
 
 $("#find-city").on("click", function (event) {
     event.preventDefault();
-
+    
     var city = $("#city-input").val();
     var searchedBtn = $("<button>");
     searchedBtn.text(city);
     $(".searched").append(searchedBtn);
-
+    
     localStorage.setItem("cityName", JSON.stringify(city));
     weatherApp(city);
 });
@@ -26,21 +26,21 @@ function weatherApp(city) {
     var queryurl =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
         city + "&appid=" + apiKey;
-
+    
     $.ajax({
         url: queryurl,
         method: "GET",
     }).then(function (response) {
         console.log(response.weather[0].icon);
-
+        
         var currentImg = $("<img>");
         currentImg.attr(
             "src",
-            `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`
+            "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
         );
-
+        //convert Kelven to farenheit https://www.checkyourmath.com/convert/temperature/kelvin_fahrenheit.php
         var actualTemp = Math.floor((response.main.temp - 273.15) * 1.8 + 32);
-
+        
         cityName.html(response.name);
         cityName.append(currentImg);
         cityTemp.html("Temp : " + actualTemp);
@@ -48,39 +48,47 @@ function weatherApp(city) {
         cityWindSpeed.html("Windspeed : " + response.wind.speed);
         cityLat = response.coord.lat;
         cityLon = response.coord.lon;
-
-
+        
+        
         var uvQueryUrl =
             "https://api.openweathermap.org/data/2.5/uvi?appid=" +
-            apiKey + "&lat=" + cityLat + "&lon=" + cityLon;
+            apiKey +
+            "&lat=" +
+            cityLat +
+            "&lon=" +
+            cityLon;
         $.ajax({
             url: uvQueryUrl,
             method: "GET",
-
-        }).then(function (uvAmount) {
-
-
-            cityUvIndex.html("UV Index : " + uvAmount.value);
-
+            
+        }).then(function (uvRes) {
+            
+            
+            cityUvIndex.html("UV Index : " + uvRes.value);
+            
             var fiveDayQueryUrl =
                 "https://api.openweathermap.org/data/2.5/forecast?q=" +
                 city + "&appid=" + apiKey;
             $.ajax({
                 url: fiveDayQueryUrl,
                 method: "GET",
-            }).then(function (fiveDayForecase) {
+            }).then(function (fiveDayForecast) {
                 $(".fiveDayRow").empty();
-
+                
                 // for the five day forcast
-
-                for (var i = 0; i < fivedayRes.list.length; i++) {
-                    if (fivedayRes.list[i].dt_txt.indexOf("12:00:00") !== -1) {
-                        var date = fivedayRes.list[i].dt_txt;
+                
+                for (var i = 0; i < fiveDayForecast.list.length; i++) {
+                    //if five day forecast at index i search element 12:00:00 is not equal to -1
+                    if (fiveDayForecast.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                        var date = fiveDayForecast.list[i].dt_txt;
                         date = moment.parseZone(date).format("MMM Do");
-                        var tempF = Math.floor((fivedayRes.list[i].main.temp - 273.15) * 1.8 + 32);
-
-
-                        var humidityFive = fivedayRes.list[i].main.humidity;
+                        var tempF = Math.floor(
+                            //convert Kelven to farenheit https://www.checkyourmath.com/convert/temperature/kelvin_fahrenheit.php
+                            (fiveDayForecast.list[i].main.temp - 273.15) * 1.8 + 32
+                        );
+                        
+                        
+                        var humidityFive = fiveDayForecast.list[i].main.humidity;
                         var wrapper = $("<div>");
                         wrapper.attr("class", "col-md-2");
                         var fiveDate = $("<p>");
@@ -89,10 +97,10 @@ function weatherApp(city) {
                         fiveImage.attr(
                             "src",
                             "http://openweathermap.org/img/wn/" +
-                            fivedayRes.list[i].weather[0].icon +
+                            fiveDayForecast.list[i].weather[0].icon +
                             ".png"
                         );
-
+                        
                         var fiveTemp = $("<p>");
                         fiveTemp.text("Temp: " + tempF);
                         var fiveHumidity = $("<p>");
