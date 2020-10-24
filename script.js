891efcc5ac1d291708c0424e8bb2c5552b3e03d7
Josh Wickster <joshwickster@gmail.com>
2020-10-24 15:45:04 -0600

    edits

--- script.js
+++ script.js
@@ -11,4 +11,21 @@ var cityLatitude = "";
 //JSON parse
 var localStorageCity = JSON.parse(localStorage.getItem("cityName"));
 
-//function call
+//Logic
+
+//Event handler to populate when searchButton is pressed
+$("#find-city").on("click", function (event) {
+    event.preventDefault();
+    //create city and link to city-input id
+    var city = $("#city-input");
+    //create button tag and assign to searchButton
+    var searchButton = ("<button>");
+    //Search button text city
+    searchButton.text(city);
+    //Stringify the city input to parse by JSON
+    localStorage.setItem("cityName", JSON.stringify(city));
+
+    //TODO call function
+});
+
+function
