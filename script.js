072a5b36e679dbbbbf229dfa40a142e457e7f125
Josh Wickster <joshwickster@gmail.com>
2020-10-24 14:06:48 -0600

    edits

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
 //Linking variables to jQuery
 //capturing Name, temp, humidity, wind speed, UV index, long/latitude
-var cityName = $(".");
-var cityTemperature = $(".");
-var cityHumidity = $(".");
-var cityWindSpeed = $(".");
-var cityUVIndex = $(".");
-var cityLongitude = $(".");
-var cityLatitude = $(".");
+var cityUVIndex = $(".uvIndex");
+var cityName = $(".temp");
+var cityHumidity = $(".humidity");
+var cityWindSpeed = $(".windSpeed");
+var cityTemperature = $(".temp");
+var cityLongitude = "";
+var cityLatitude = "";
 
 //JSON parse
-var localStorageCity = JSON.parse(localStorage.getItem(cityName));
+var localStorageCity = JSON.parse(localStorage.getItem("cityName"));
 
 //function call
-
